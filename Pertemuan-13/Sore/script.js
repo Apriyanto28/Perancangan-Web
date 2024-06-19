1ee736c9ec169b9b7f22fe04324ec4bbebac34e3
Apriyanto Halim <apriyanto.halim@mikroskil.ac.id>
2024-06-19 18:16:45 +0700

    Menambahkan contoh kode Pertemuan-13 Sore Teori

---
+++ Pertemuan-13/Sore/script.js
@@ -0,0 +1,11 @@
+var hsl = document.getElementById("hasil");
+        //hsl.innerHTML = "Apriyanto ";
+        //hsl.innerHTML += "Halim";
+        hsl.innerHTML = "";
+        for(let i =1;i<=10;i++){
+            for(let j = 1;j<=i;j++){
+                //hsl.innerHTML += i + " ";
+                console.log(i);
+            }
+            //hsl.innerHTML += "<br>";
+        }
